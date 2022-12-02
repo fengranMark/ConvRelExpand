2e3501ca7da46f9b0f8c1c0a3477691d8dd39b5d
FungJin Mok <464765199@qq.com>
2022-12-02 13:37:22 -0500

    Add files via upload

---
+++ bm25_baseline/convert_to_pyserini_format.py
@@ -0,0 +1,42 @@
+import csv
+import json
+import os
+from argparse import ArgumentParser
+
+from tqdm import tqdm
+
+WIKI_FILE = "qrecc_collection.tsv"
+OUTPUT_FILE = "bm25_collection/qrecc_collection.jsonl"
+
+id_col= 0
+text_col= 1
+title_col = 2
+
+def main(wiki_file, output_file):
+
+    '''
+    with open(wiki_file, 'r') as input:
+        reader = csv.reader(input, delimiter="\t")
+        with open(output_file, 'w') as output:
+            for i, row in enumerate(tqdm(reader)):
+                if len(row[text_col]) == 0:
+                    continue
+                text = row[text_col]
+                obj = {"id": f"doc{i}", "contents": text}
+                output.write(json.dumps(obj, ensure_ascii=False) + '\n')
+    '''
+    with open(output_file, 'w') as output:
+        for line in tqdm(open(WIKI_FILE, "r")):
+            try:
+                pid, passage = line.strip().split('\t')
+                obj = {"id": "doc" + str(pid), "contents": passage}
+                output.write(json.dumps(obj, ensure_ascii=False) + '\n')
+            except:
+                continue
+            #pid = int(line.strip().split('\t')[0])
+            #passage = ""
+            #bad_passage_set.add(pid)
+
+if __name__ == "__main__":
+    main(WIKI_FILE, OUTPUT_FILE)
+
